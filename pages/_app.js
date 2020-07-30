b5f78eaa8c7e6db9218353e9cb3d4ab329f743c9
swimjiy <swimjiy@gmail.com>
2020-07-30 21:33:57 +0900

    Create _app.js file

---
+++ pages/_app.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import App, { Container } from 'next/app';
+import { ThemeProvider } from '@material-ui/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { createMuiTheme } from '@material-ui/core';
+
+export default class RootApp extends App {
+    constructor(props){
+        super(props)
+    }
+    componentDidMount() {
+        const jssStyles = document.querySelector('#jss-server-side');
+        if (jssStyles) {
+            jssStyles.parentNode.removeChild(jssStyles);
+        }
+    }
+    static async getInitialProps({ Component, ctx }) {
+        let pageProps = {}
+        if (Component.getInitialProps) {
+            pageProps = await Component.getInitialProps(ctx)
+        }
+        return { pageProps }
+    }
+    render() {
+        const { Component, pageProps } = this.props;
+        const theme = createMuiTheme({
+            palette: {
+                background: {
+                    default: '#FFF',
+                },
+                primary: {
+                    main: '#673ab7',
+                }
+            }
+        })
+        return (
+            <Container>
+                <ThemeProvider theme={theme}>
+                    <CssBaseline>
+                        <Component {...pageProps} />
+                    </CssBaseline>
+                </ThemeProvider>
+            </Container>
+        )
+    }
+}
