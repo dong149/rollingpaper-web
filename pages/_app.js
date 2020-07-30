b4106374d861a3e777fb1f4552f84f7e4581e2c6
swimjiy <swimjiy@gmail.com>
2020-07-30 23:10:19 +0900

    Format _app style

--- pages/_app.js
+++ pages/_app.js
@@ -5,42 +5,42 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import { createMuiTheme } from '@material-ui/core';
 
 export default class RootApp extends App {
-    constructor(props){
-        super(props)
+  constructor(props) {
+    super(props);
+  }
+  componentDidMount() {
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentNode.removeChild(jssStyles);
     }
-    componentDidMount() {
-        const jssStyles = document.querySelector('#jss-server-side');
-        if (jssStyles) {
-            jssStyles.parentNode.removeChild(jssStyles);
-        }
+  }
+  static async getInitialProps({ Component, ctx }) {
+    let pageProps = {};
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
     }
-    static async getInitialProps({ Component, ctx }) {
-        let pageProps = {}
-        if (Component.getInitialProps) {
-            pageProps = await Component.getInitialProps(ctx)
-        }
-        return { pageProps }
-    }
-    render() {
-        const { Component, pageProps } = this.props;
-        const theme = createMuiTheme({
-            palette: {
-                background: {
-                    default: '#FFF',
-                },
-                primary: {
-                    main: '#673ab7',
-                }
-            }
-        })
-        return (
-            <Container>
-                <ThemeProvider theme={theme}>
-                    <CssBaseline>
-                        <Component {...pageProps} />
-                    </CssBaseline>
-                </ThemeProvider>
-            </Container>
-        )
-    }
+    return { pageProps };
+  }
+  render() {
+    const { Component, pageProps } = this.props;
+    const theme = createMuiTheme({
+      palette: {
+        background: {
+          default: '#FFF',
+        },
+        primary: {
+          main: '#673ab7',
+        },
+      },
+    });
+    return (
+      <Container>
+        <ThemeProvider theme={theme}>
+          <CssBaseline>
+            <Component {...pageProps} />
+          </CssBaseline>
+        </ThemeProvider>
+      </Container>
+    );
+  }
 }
