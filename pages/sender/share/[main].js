e086f9161b799cc11f684703bbf278570fe7686f
DANNIEL <donghoon149@gmail.com>
2020-08-04 14:29:02 +0900

    fix bug #26

--- pages/sender/share/[main].js
+++ pages/sender/share/[main].js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import Layouts from '../../components/Layouts';
-import Buttons from '../../components/Buttons';
+import Layouts from '../../../components/Layouts';
+import Buttons from '../../../components/Buttons';
 
 const Share = (props) => {
   const { name, num } = props;
