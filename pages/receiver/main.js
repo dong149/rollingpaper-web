12c42b7aaf4ae2753b3b1ce3227d93aecf5b49cf
DANNIEL <donghoon149@gmail.com>
2020-08-08 17:08:21 +0900

    add confetti feature

--- pages/receiver/main.js
+++ pages/receiver/main.js
@@ -6,6 +6,8 @@ import StickyFooter from '../../components/StickyFooter';
 import Cards from '../../components/Cards';
 import Buttons from '../../components/Buttons';
 import { exportComponentAsPNG } from '../../functions';
+
+import Confetti from 'react-confetti';
 const cardList = Array(30).fill('카드'); // 임시 배열
 
 const useStyles = makeStyles((theme) => ({
@@ -43,33 +45,37 @@ const useStyles = makeStyles((theme) => ({
 const Main = () => {
   const classes = useStyles();
   const componentRef = useRef();
+  const layoutRef = useRef();
   return (
-    <Layouts className={classes.root} bgColor="#F7F7F7">
-      <Header>
-        <div>
-          <h2 className={classes.title}>to. 류동훈님</h2>
-          <p className={classes.subtitle}>
-            {cardList.length
-              ? `총 ${cardList.length}명에게 축하를 받았어요!`
-              : `아직 아무도 작성하지 않았어요!`}
-          </p>
+    <>
+      <Confetti width={layoutRef.width} height={layoutRef.height} />
+      <Layouts className={classes.root} bgColor="#F7F7F7" ref={layoutRef}>
+        <Header>
+          <div>
+            <h2 className={classes.title}>to. 류동훈님</h2>
+            <p className={classes.subtitle}>
+              {cardList.length
+                ? `총 ${cardList.length}명에게 축하를 받았어요!`
+                : `아직 아무도 작성하지 않았어요!`}
+            </p>
+          </div>
+          {/* TODO: 주인공 페이지에서 공유하기 버튼 임시 삭제 */}
+          {/* <button className={classes.buttonSmall}>공유하기</button> */}
+        </Header>
+        <div className={classes.cardWrapper} ref={componentRef}>
+          {cardList.length ? (
+            <Cards content={cardList} linked />
+          ) : (
+            <div>더미</div>
+          )}
         </div>
-        {/* TODO: 주인공 페이지에서 공유하기 버튼 임시 삭제 */}
-        {/* <button className={classes.buttonSmall}>공유하기</button> */}
-      </Header>
-      <div className={classes.cardWrapper} ref={componentRef}>
-        {cardList.length ? (
-          <Cards content={cardList} linked />
-        ) : (
-          <div>더미</div>
-        )}
-      </div>
-      <StickyFooter>
-        <Buttons full onClick={() => exportComponentAsPNG(componentRef)}>
-          이미지 다운받기
-        </Buttons>
-      </StickyFooter>
-    </Layouts>
+        <StickyFooter>
+          <Buttons full onClick={() => exportComponentAsPNG(componentRef)}>
+            이미지 다운받기
+          </Buttons>
+        </StickyFooter>
+      </Layouts>
+    </>
   );
 };
 
