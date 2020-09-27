5a8e3926205afa9c405abb9dfcc11e21599746c4
Kong Chae Won <chaewonkong@gmail.com>
2020-09-28 01:28:49 +0900

    Use apilb

--- services/rollingService.js
+++ services/rollingService.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // let BASE_URL = "https://donghoon.tk";
 
-let BASE_URL = 'https://apis.rollingpaper.site';
+let BASE_URL = 'https://apilb.rollingpaper.site';
 
 const baseAPI = axios.create({
   baseURL: BASE_URL,
